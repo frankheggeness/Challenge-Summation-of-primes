2338f11cc73ffe8aec33bdf1d4dc505566719c20
Frank <fr.heggeness@gmail.com>
2019-04-24 09:24:45 -1000

    primes

--- summationOfPrimes.js
+++ summationOfPrimes.js
@@ -1,3 +1,19 @@
 module.exports = function(limit) {
-  // do work here
-}
+  let sum = 0;
+  function isPrime(value) {
+    for (let i = 2; i < value; i++) {
+      if (value % i == 0) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  for (let i = 2; i < limit; i++) {
+    if (isPrime(i)) {
+      sum += i;
+    }
+  }
+
+  return sum;
+};
